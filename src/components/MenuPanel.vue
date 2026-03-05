f31b94c630bb2aeea536c07e3b10fa5609c967fd
idlan9114 <idlan9114@gmail.com>
2026-03-05 16:25:35 +0800

    updated

--- src/components/MenuPanel.vue
+++ src/components/MenuPanel.vue
@@ -1,16 +1,18 @@
 <script setup>
 import { ref } from 'vue';
 const isOpen = ref(false);
+import Sidebar from './Sidebar.vue';
+
 </script>
 
 <template>
   <div class="relative">
 
-    <button @click="isOpen = true" class="flex flex-col justify-between w-6 h-5 p-0.5">
+    <b-button @click="isOpen = true" class="flex flex-col justify-between w-6 h-5 p-0.5">
       <span class="block w-full h-0.5 bg-white"></span>
       <span class="block w-full h-0.5 bg-white"></span>
       <span class="block w-full h-0.5 bg-white"></span>
-    </button>
+    </b-button>
 
 
     <div v-if="isOpen" class="fixed inset-0 overflow-hidden z-50">
@@ -18,7 +20,7 @@ const isOpen = ref(false);
 
       <Transition name="slide">
         <div v-if="isOpen" class="absolute left-0 top-0 h-full w-64 bg-white shadow-lg p-4">
-          <Menu/> 
+          <Sidebar/> 
         </div>
       </Transition>
     </div>
